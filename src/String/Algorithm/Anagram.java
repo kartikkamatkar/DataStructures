dcf2f987e65b045e3dd40877da9411439af28fa1
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-04-27 10:56:42 +0530

    Today i Solve Only String Problem

--- src/String/Algorithm/Anagram.java
+++ src/String/Algorithm/Anagram.java
@@ -1,4 +1,35 @@
 package String.Algorithm;
+//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//
+//
+//
+//        Example 1:
+//
+//Input: s = "anagram", t = "nagaram"
+//
+//Output: true
+
 
 public class Anagram {
+    public static void main(String[] args) {
+        String s="anagram";
+        String t ="nagaram";
+        if(s.length()!=t.length()){
+            System.out.println(false);
+            return;
+        }
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int i =0;i<26;i++){
+            if(freq[i]!=0){
+                System.out.println(false);
+                return;
+            }
+
+        }
+        System.out.println(true);
+    }
 }
