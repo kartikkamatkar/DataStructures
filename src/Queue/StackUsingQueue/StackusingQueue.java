3b8ac742e7a6ac281714be681829fe7b7462e762
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-04-29 15:28:01 +0530

    class add

---
+++ src/Queue/StackUsingQueue/StackusingQueue.java
@@ -0,0 +1,20 @@
+package Queue.StackUsingQueue;
+class Stack{
+    int arr1[],arr2[];
+    int front1,rear1,front2,rear2,len1,len2;
+    Stack(int size1,int size2){
+        len1=size1;
+        arr1=new int[len1];
+        arr2=new int [len2];
+        front1=-1;
+        front2=-1;
+        rear1=-1;
+        rear2=-1;
+    }
+}
+public class StackusingQueue
+{
+    public static void main(String[] args) {
+
+    }
+}
