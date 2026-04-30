827097e286d4f52df4f83b9ebdf9692880434e0f
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-04-30 23:54:24 +0530

    solve another problem

--- src/String/Algorithm/ReverseStringII.java
+++ src/String/Algorithm/ReverseStringII.java
@@ -1,27 +1,28 @@
 package String.Algorithm;
 //Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
-//
 //If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
-//
-//
-//
 //Example 1:
-//
 //Input: s = "abcdefg", k = 2
 //Output: "bacdfeg"
 //Example 2:
-//
-//Input: s = "abcd", k = 2
+//Input: s =gi "abcd", k = 2
 //Output: "bacd"
 public class ReverseStringII {
     public static void main(String[] args) {
-        String s ="abcdefg";//output :"bacdfeg"
-        int k =2;
-        for(int i =0;i<s.length();i+=k*i){
-            int l =0;
-            int r =Math.min(k+i-1,s.length()-1);
-            while(l<r)
+        String s = "abcdefg";
+        char[] arr = s.toCharArray();
+        int k = 2;
+        for (int i = 0; i < arr.length; i += 2 * k) {
+            int l = i;
+            int r = Math.min(i + k - 1, arr.length - 1);
+            while (l < r) {
+                char temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+                l++;
+                r--;
+            }
         }
-
+        System.out.println(new String(arr));
     }
 }
