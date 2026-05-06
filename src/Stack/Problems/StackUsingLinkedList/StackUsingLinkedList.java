39942c9956a194efb809569caadb8d9abb5462c8
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-05-06 16:34:50 +0530

    Stack using Linked List Problem Solved

---
+++ src/Stack/Problems/StackUsingLinkedList/StackUsingLinkedList.java
@@ -0,0 +1,23 @@
+package Stack.Problems.StackUsingLinkedList;
+
+import java.util.Stack;
+class Node{
+    int data;
+    Node next;
+    Node(int val)
+    {
+        data=val;
+        next=null;
+    }
+}
+class  StackinLL
+{
+    Node head;
+
+}
+public class StackUsingLinkedList
+{
+    public static void main(String[] args) {
+
+    }
+}
