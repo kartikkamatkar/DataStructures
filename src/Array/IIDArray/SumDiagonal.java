dc47bef4e90846c1591a5d8ed47865b00ef29760
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-05-18 23:27:13 +0530

    Changes

--- src/Array/IIDArray/SumDiagonal.java
+++ src/Array/IIDArray/SumDiagonal.java
@@ -7,12 +7,13 @@ public class SumDiagonal
         int n =matrix.length;
         for(int i=0;i<n;i++){
             for(int j =0;j<n;j++){
-                if(matrix[i]==matrix[j]){
+                if(i ==j ){
                     sum += matrix[i][j];
                 }
                 else if (i+j==n-1){
                     sum+=matrix[i][j];
                 }
+
 
             }
         }
