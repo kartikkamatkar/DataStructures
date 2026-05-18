ffed20a469c78736fb832ba1cafda04a8ac07e0a
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-05-18 13:33:33 +0530

    Counting Sort added in ls branch

---
+++ src/Array/ArrayAlgorithm/Sorting/CountingSort.java
@@ -0,0 +1,16 @@
+package Array.ArrayAlgorithm.Sorting;
+
+public class CountingSort
+{
+    public static int [] countingSort(int arr[]){
+        int count =0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i])
+        }
+        return arr[i];
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,2,4,5,6,2,5};
+
+    }
+}
