e4a5120370b8ca972cbab264279abddc658c258b
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-05-25 16:04:18 +0530

    Another problem is added

--- src/Array/ArrayAlgorithm/Searching/AdvBinarySearch.java
+++ src/Array/ArrayAlgorithm/Searching/AdvBinarySearch.java
@@ -1,8 +1,55 @@
 package Array.ArrayAlgorithm.Searching;
+//There is an integer array nums sorted in ascending order (with distinct values).
+//
+//Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+//
+//Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+//
+//You must write an algorithm with O(log n) runtime complexity.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [4,5,6,7,0,1,2], target = 0
+//Output: 4
 
 public class AdvBinarySearch
 {
+    public static int BinarySearch(int []nums,int target){
+        int l =0;
+        int r = nums.length-1;
+        while(l<=r){
+            int mid =l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target&&target<nums[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l =mid+1;
+
+                }
+            }
+            else{
+                if(nums[mid]<target&& target<=nums[r])
+                {
+                    l =mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+
+    }
     public static void main(String[] args) {
+    int nums[]={4,5,6,7,0,1,2};
+    int target =0;
+        System.out.println(BinarySearch(nums,target));
 
     }
 }
