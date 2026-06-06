402ca2cf7cf2e610c2e7da3854b7726d41062489
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-06-06 11:36:03 +0530

    Xormax Problem Solve by Brute force

---
+++ src/Array/XORProblemMax.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class XORProblemMax
+{
+    static int maxXor(int []arr){
+       int max =Integer.MIN_VALUE;
+       for(int i =0;i<arr.length;i++){
+           for(int j =i+1;j<arr.length;j++){
+               max=Math.max(max,arr[i]^arr[j]);
+           }
+       }
+        return max;
+    }
+    public static void main(String[] args) {
+ int arr[]={3,10,5,25,2,8};
+        System.out.println(maxXor(arr));
+    }
+}
